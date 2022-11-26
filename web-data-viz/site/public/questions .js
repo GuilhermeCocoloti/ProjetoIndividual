a10d1bdfce663a4932c88c71dbb6e65611303034
GuilhermeCocoloti <guilherme.moraes@sptech.school>
2022-11-26 18:14:30 -0300

    Site v6

---
+++ web-data-viz/site/public/questions .js
@@ -0,0 +1,60 @@
+export default [
+
+    {
+
+        question: "Em uma situação de 1vs1 (jogador) a partir do meio-campo, O que você faria?",
+        answers: [
+            { option: "Tento ganhar na corrida, deixando o marcador para trás." },
+            { option: "Divido no corpo e procuro um passe." },
+            { option: "Driblo o marcador e faço uma arrancada" },
+        ],
+    },
+    {
+        question: "Em uma jogada de escanteio, Você prefere:",
+        answers: [
+            { option: "Cruzar na área e dividir no cabeceio." },
+            { option: "Passe curto, driblando a marcação." },
+            { option: "Passe curto, tocando a bola até um atacante forte para aguentar jogo de corpo na área." },
+        ],
+    },
+    {
+        question: "Em relação a disputas na corrida, você prefere que o jogador faça:",
+        answers: [
+
+            { option: "Uma arrancada explosiva, caindo a velocidade aos poucos" },
+            { option: "Uma corrida mais lenta, mas que vai ganhando bastante velocidade com o tempo capaz de superar o adversário." },
+            { option: "Arrancar em uma velocidade equilibrada e mantê-la até o final." },
+
+        ],
+    },
+    {
+        question: "Se fosse para escolher, você escolheria:",
+        answers: [
+            { option: "Um jogador entre 1,70 e 1,80, ágil porém fraco." },
+            { option: "Um jogador entre 1,75 e 1,87, nem muito ágil e nem tão pesado." },
+            { option: "Um jogador acima de 1,87, forte porém pesado." },
+        ],
+
+    },
+    {
+        question: "Você se sai melhor com jogador que tenha:",
+        answers: [
+            { option: "Um jogador de 3 estrelas de finta, com 5 estrelas de perna ruim." },
+            { option: "Um jogador de 5 estrelas de finta, com 3 estrelas de perna ruim." },
+            { option: "Um jogador de 4 estrelas de finta, com 4 estrelas de perna ruim." },
+
+        ],
+    },
+    {
+        question: "O que você prefere?",
+        answers: [
+            { option: "Um jogador que corra bastante e ágil, porém com um chute abaixo de 81." },
+            { option: "Um jogador com um chute totalmente forte e preciso, porém com uma agilidade baixa e velocidade abaixo de 83." },
+            { option: "Um jogador com velocidade, chute e agilidade balanceados." },
+
+        ],
+    },
+
+
+
+] 
