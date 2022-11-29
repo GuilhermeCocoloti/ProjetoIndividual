e8e4f94434881fa51e7ca3714e0fb400982a00a0
GuilhermeCocoloti <guilherme.moraes@sptech.school>
2022-11-29 18:23:11 -0300

    Script BD v2

--- Banco de Dados/Script.sql
+++ Banco de Dados/Script.sql
@@ -2,17 +2,22 @@ CREATE DATABASE ultimate;
 
 USE ultimate;
 
+
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
+	senha VARCHAR(50),
+    fkPref int,
+    foreign key (fkPref) references preferencia (idPref)
 );
 
 create table preferencia (
 idPref INT PRIMARY KEY auto_increment,
 bodytype varchar(15),
-accelerate varchar(15));
+constraint chkbodytype check (bodytype in ('Lean','Average','Stocky')),
+accelerate varchar(15),
+constraint chkaccelerate check (accelerate in ('Explosive','Controlled','Lengthy')));
 
 create table quiz (
 idQuiz int primary key auto_increment,
@@ -23,6 +28,21 @@ explosive int,
 controlled int,
 lengthy int);
 
+insert into usuario values 
+(null, 'Guilherme', 'guiiii@gmail.com', '1234', 1);
+
+insert into preferencia values 
+(null, 'lean', 'explosive');
+
 select * from usuario;
+select * from preferencia;
+
+select * from usuario 
+join preferencia where 
+fkpref = idPref;
+
 
 delete from usuario where id between 1 and 10;
+
+drop table usuario;
+drop table preferencia;
